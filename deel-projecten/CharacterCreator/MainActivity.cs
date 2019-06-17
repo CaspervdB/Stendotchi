3ca5fab4e9aad14b348daf5f8f12ba4727c0bdcf
StudentNiels <43601448+StudentNiels@users.noreply.github.com>
2019-06-17 12:51:13 +0200

    CharacterCreator references werken
    1 error "linkassemblies" failed

--- deel-projecten/CharacterCreator/MainActivity.cs
+++ deel-projecten/CharacterCreator/MainActivity.cs
@@ -1,21 +1,37 @@
-﻿using Android.App;
+﻿using System;
+
+using System.Web;
+using Android.Systems;
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 
 namespace CharacterCreator
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "CharacterCreator", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var charCreatorHairButtTerug = FindViewById<ImageButton>(Resource.Id.charCreatorHairButtTerug);
+
         }
+
+        private object FindViewById<T>(object charCreatorHairButtTerug)
+        {
+            throw new NotImplementedException();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
