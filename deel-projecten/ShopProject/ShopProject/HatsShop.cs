73333d1a4f232888956f1c3bee961ceb43501505
Danny de Vries <danny.de.vries@student.nhlstenden.com>
2019-06-17 11:51:39 +0200

    Shopproject danny

---
+++ deel-projecten/ShopProject/ShopProject/HatsShop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ShopProject
+{
+    [Activity(Label = "HatsShop")]
+    public class HatsShop : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.HatsLayout);
+
+            Button button4 = FindViewById<Button>(Resource.Id.FirstHats);
+
+            button4.Click += delegate
+            {
+                StartActivity(typeof(ShopMain));
+            };
+
+
+            // Create your application here
+        }
+    }
+}
